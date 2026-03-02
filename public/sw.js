6180f5eed15baae66b58ca0f794d1cac2f363915
CallbridgeWebDev <101778509+callbridgewebdev@users.noreply.github.com>
2026-03-02 18:18:30 +0800

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+// Service Worker for PWA Installability
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Basic fetch handler to allow install prompt
+  event.respondWith(fetch(event.request));
+});
